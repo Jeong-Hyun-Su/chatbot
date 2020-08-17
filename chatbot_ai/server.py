e67099dd55b8b1fb2a72b47a7ce45637fcc35864
Jeong-Hyun-Su <mses1572@naver.com>
2020-08-17 10:43:04 +0900

    update

---
+++ chatbot_ai/server.py
@@ -0,0 +1,40 @@
+from flask import Flask, make_response
+from flask_restful import request, Resource, Api
+from json import dumps
+import urllib
+import chatbot as cb
+
+
+app = Flask(__name__)
+api = Api(app)
+
+
+# Chatbot Class
+class Chatbot(Resource):
+    # GET 메소드
+    def get(self):
+        sentence = request.args.get('sentence', '')
+        
+        if sentence and sentence != "":
+            bot_response = str(cb.chatbot.get_response(sentence))
+        else:       # 문자열이 없을경우, ''의 경우도 해당
+            bot_response = ""
+            
+        # bot_response - 챗봇 대답
+        # okay - 성공적으로 챗봇이 작동하였는지 유무, 링크가 정상적인지 체크
+        # question - 사용자의 질문
+        data = { 'answer' : bot_response }
+        dump_data = dumps(data, ensure_ascii=False)
+
+        print(sentence)
+        print(bot_response)
+        return make_response(dump_data)
+        
+        # return bot_response
+ 
+
+api.add_resource(Chatbot, '/chatbot')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
